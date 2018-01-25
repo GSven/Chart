85bdfab36f2c46caf8c0ac7578419ec701677e31
good-good-study <1183718777@qq.com>
2018-01-25 23:30:25 +0800

    添加了横向滚动的Text

--- app/src/main/java/com/sxt/chart/activity/ScrollTextActivity.java
+++ app/src/main/java/com/sxt/chart/activity/ScrollTextActivity.java
@@ -4,8 +4,12 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.widget.TextView;
 
+import com.sxt.chart.App;
 import com.sxt.chart.R;
+import com.sxt.chart.utils.ToastUtil;
 import com.sxt.chart.view.HorizontalselectedView;
+import com.sxt.chart.view.ScrollPickerView;
+import com.sxt.chart.view.StringScrollPicker;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,6 +28,7 @@ public class ScrollTextActivity extends BaseActivity {
         final TextView select = findViewById(R.id.select);
         HorizontalselectedView hsView = findViewById(R.id.scrollText);
 
+
         List<String> strings = new ArrayList<>();
         for (int i = 18; i < 100; i++) {
             strings.add(String.valueOf(i));
@@ -37,5 +42,15 @@ public class ScrollTextActivity extends BaseActivity {
             }
         });
 
+        StringScrollPicker scrollPicker = findViewById(R.id.scrollPicker);
+        List<CharSequence> newList = new ArrayList<>();
+        newList.addAll(strings);
+        scrollPicker.setData(newList);
+        scrollPicker.setOnSelectedListener(new ScrollPickerView.OnSelectedListener() {
+            @Override
+            public void onSelected(ScrollPickerView scrollPickerView, int position) {
+                ToastUtil.showToast(App.getCtx(), (String) scrollPickerView.getSelectedItem());
+            }
+        });
     }
 }
