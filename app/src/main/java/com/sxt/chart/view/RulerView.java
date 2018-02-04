42372ec243bb72648681b41bfe8136ae0dd805b2
good-good-study <1183718777@qq.com>
2018-02-04 16:18:05 +0800

    添加了横向滚动的Text

--- app/src/main/java/com/sxt/chart/view/RulerView.java
+++ app/src/main/java/com/sxt/chart/view/RulerView.java
@@ -7,13 +7,14 @@ import android.graphics.Path;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewConfiguration;
+import android.view.ViewParent;
 import android.widget.Scroller;
 
 import com.sxt.chart.R;
-import com.sxt.chart.utils.LogUtil;
 import com.sxt.chart.utils.Px2DpUtil;
 
 import java.util.ArrayList;
@@ -54,25 +55,25 @@ public class RulerView extends View {
     private Path indicatorPath;
 
 
+    private GestureDetector mGestureDetector;
     private Scroller mScroller;
     private int touchSlop;
     private float leftBorder, rightBorder;
     private int rulerWidth;
-    private float moveLength;
-    private float downX;
-    private float downY;
-    private float moveX;
-    private float lastMoveX;
-    private float moveY;
-    private float upX;
-    private float upY;
-    public boolean onScaleTouch;
-    private boolean onIndicatorTouch;
+    private float mLastMoveY; // 触摸的坐标y
+    private float mLastMoveX; // 触摸的坐标X
+    private boolean mIsMovingCenter; // 是否正在滑向中间
+    // 可以把scroller看做模拟的触屏滑动操作，mLastScrollY为上次触屏滑动的坐标
+    private int mLastScrollY = 0; // Scroller的坐标y
+    private int mLastScrollX = 0; // Scroller的坐标x
+
+    private boolean mDisallowTouch = false; // 不允许触摸
+
 
     private int page;
     private List<String> data;
     private float minValue = 0, maxValue = 101;
-    private float itemWidth = Px2DpUtil.dip2px(getContext(), 6);
+    private float mItemWidth = Px2DpUtil.dip2px(getContext(), 6);
     private List<float[]> scaleLines = new ArrayList<>();
     private List<String[]> scaleTexts = new ArrayList<>();
     private OnSelectChangeListenrer onSelectChangeListener;
@@ -98,13 +99,14 @@ public class RulerView extends View {
         indicatorPath = new Path();
 
         mScroller = new Scroller(getContext());
+        mGestureDetector = new GestureDetector(getContext(), new FlingOnGestureListener());
         touchSlop = ViewConfiguration.get(getContext()).getScaledPagingTouchSlop();
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        rulerWidth = (int) ((maxValue + 1 - minValue) * itemWidth + basePadding);
+        rulerWidth = (int) ((maxValue + 1 - minValue) * mItemWidth + basePadding);
         if (rulerWidth < widthMeasureSpec) {
             rulerWidth = widthMeasureSpec;
         }
@@ -121,8 +123,8 @@ public class RulerView extends View {
             endY = getPaddingTop() + basePadding;
             page = (int) (rulerWidth / (endX - startX));
 
-            leftBorder = startX;
-            rightBorder = leftBorder + (maxValue - minValue - 1) * itemWidth;
+            leftBorder = startX - (maxValue - minValue - 1) * mItemWidth / 2;
+            rightBorder = endX + rulerWidth;
         }
     }
 
@@ -148,33 +150,33 @@ public class RulerView extends View {
     private void drawIndicator(Canvas canvas) {
         //初始化时，将指示器绘制在中间位置
         if (scaleLines.size() > 0) {
-            if (moveX == 0.0 || moveY == 0.0) {
-                int index = scaleLines.size() / 2;
-                float[] floats = scaleLines.get(index);
-                canvas.drawLine(floats[0], floats[1], floats[2], floats[3], indicatorPaint);
-                indicatorPath.reset();
-                indicatorPath.moveTo(floats[0], floats[1]);
-                indicatorPath.lineTo(floats[0] + indicatorWidth, floats[1] - indicatorHigth);
-                indicatorPath.lineTo(floats[0] - indicatorWidth, floats[1] - indicatorHigth);
-                indicatorPath.close();
-                canvas.drawPath(indicatorPath, indicatorPaint);
-                if (onSelectChangeListener != null) {
-                    onSelectChangeListener.onSelectChange(this, (floats[0] - leftBorder) / itemWidth);
-                }
-            } else {
-                //查找触摸位置
-                float[] xy = getOnTouch();
-                canvas.drawLine(xy[0], xy[1], xy[2], xy[3], indicatorPaint);
-                indicatorPath.reset();
-                indicatorPath.moveTo(xy[0], xy[1]);
-                indicatorPath.lineTo(xy[0] + indicatorWidth, xy[1] - indicatorHigth);
-                indicatorPath.lineTo(xy[0] - indicatorWidth, xy[1] - indicatorHigth);
-                indicatorPath.close();
-                canvas.drawPath(indicatorPath, indicatorPaint);
-                if (onSelectChangeListener != null) {
-                    onSelectChangeListener.onSelectChange(this, (xy[0] - leftBorder) / itemWidth);
-                }
+//            if (mLastMoveX == 0.0) {
+//                int index = scaleLines.size() / 2;
+//                float[] floats = scaleLines.get(index);
+//                canvas.drawLine(floats[0], floats[1], floats[2], floats[3], indicatorPaint);
+//                indicatorPath.reset();
+//                indicatorPath.moveTo(floats[0], floats[1]);
+//                indicatorPath.lineTo(floats[0] + indicatorWidth, floats[1] - indicatorHigth);
+//                indicatorPath.lineTo(floats[0] - indicatorWidth, floats[1] - indicatorHigth);
+//                indicatorPath.close();
+//                canvas.drawPath(indicatorPath, indicatorPaint);
+//                if (onSelectChangeListener != null) {
+//                    onSelectChangeListener.onSelectChange(this, (floats[0] - leftBorder) / mItemWidth);
+//                }
+//            } else {
+            //查找触摸位置
+            float[] xy = getOnTouch();
+            canvas.drawLine(xy[0], xy[1], xy[2], xy[3], indicatorPaint);
+            indicatorPath.reset();
+            indicatorPath.moveTo(xy[0], xy[1]);
+            indicatorPath.lineTo(xy[0] + indicatorWidth, xy[1] - indicatorHigth);
+            indicatorPath.lineTo(xy[0] - indicatorWidth, xy[1] - indicatorHigth);
+            indicatorPath.close();
+            canvas.drawPath(indicatorPath, indicatorPaint);
+            if (onSelectChangeListener != null) {
+                onSelectChangeListener.onSelectChange(this, (xy[0] - leftBorder) / mItemWidth);
             }
+//            }
         }
     }
 
@@ -182,7 +184,7 @@ public class RulerView extends View {
         if (scaleLines.size() == 0) {
             for (int i = 0; i < maxValue - minValue; i++) {
                 float[] pts = new float[4];
-                float start = leftBorder + i * itemWidth;
+                float start = startX + i * mItemWidth;
                 pts[0] = start;
                 pts[1] = endY;
                 pts[2] = start;
@@ -217,73 +219,43 @@ public class RulerView extends View {
 //
 //        RectF ova2 = new RectF(startX, startY, endX, startY);
 //        canvas.drawArc(ova2, -180, 180, false, new Paint(basePaint));
-        canvas.drawLine(startX, endY, rightBorder, endY, basePaint);
-        canvas.drawLine(startX, startY - basePadding, rightBorder, startY - basePadding, basePaint);
+        canvas.drawLine(startX, endY, startX + rulerWidth, endY, basePaint);
+        canvas.drawLine(startX, startY - basePadding, startX + rulerWidth, startY - basePadding, basePaint);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        if (mGestureDetector.onTouchEvent(event)) {
+            return true;
+        }
         switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-                downX = event.getX();
-                downY = event.getY();
-                lastMoveX = downX;
-//                if (downY < startY - (startY - endY) / 2) {
-//                    onIndicatorTouch = true;
-//                    onScaleTouch = false;
-//                } else {
-//                    onIndicatorTouch = false;
-//                    onScaleTouch = true;
-//                }
-                invalidate();
-                LogUtil.i("sxt", "down");
-                break;
-
             case MotionEvent.ACTION_MOVE:
-                moveX = event.getX();
-                moveY = event.getY();
-//                lastMoveX = event.getX();
-                invalidate();
-                getParent().requestDisallowInterceptTouchEvent(true);
-                if (moveY > startY - (startY - endY) / 2) {
-                    getParent().requestDisallowInterceptTouchEvent(false);
-                }
-//                float dis = moveX - downX;
-//                moveLength += dis;
-//                LogUtil.i("sxt", "getScrollX() = " + getScrollX());
+                float offsetX = event.getX() - mLastMoveX;
+                mLastMoveX = event.getX();
+                mLastMoveY = event.getY();
 
-//                if (Math.abs(dis) > touchSlop) {
-//                    if (onIndicatorTouch && !onScaleTouch) {
-//                        invalidate();
-//                    } else if (!onIndicatorTouch && onScaleTouch) {
-//                        if (getScrollX() + dis < leftBorder - (rightBorder - leftBorder) / 5) {
-//                            scrollTo((int) (leftBorder - (rightBorder - leftBorder) / 2), 0);
-//                            return true;
-//                        }
-//                        if (getScrollX() + dis > rightBorder) {
-//                            scrollTo((int) rightBorder, 0);
-//                            return true;
-//                        }
-//
-//                        scrollBy((int) dis, 0);
-//                        invalidate();
-//                    }
-//                }
+                if (Math.abs(offsetX) > touchSlop) {
+                    if (getScrollX() + offsetX < leftBorder) {
+                        scrollTo((int) leftBorder, 0);
+                        return true;
+                    }
+                    if (getScrollX() + offsetX > rightBorder) {
+                        scrollTo((int) rightBorder, 0);
+                        return true;
+                    }
+
+                    scrollBy((int) offsetX, 0);
+                    invalidate();
+
+                } else {
+                    return true;
+                }
 
                 break;
             case MotionEvent.ACTION_UP:
-                upX = event.getX();
-                upY = event.getY();
-                lastMoveX = upX;
-                moveX = 0;
-                moveY = 0;
-                downX = 0;
-                downY = 0;
-
-                onIndicatorTouch = false;
-                onScaleTouch = false;
-                getParent().requestDisallowInterceptTouchEvent(false);
-                LogUtil.i("sxt", "ACTION_UP");
+                mLastMoveX = event.getX();
+                mLastMoveY = event.getY();
+//                mScroller.startScroll((int) leftBorder, 0, (int) rightBorder, 0);
                 break;
         }
         return super.onTouchEvent(event);
@@ -299,14 +271,63 @@ public class RulerView extends View {
         }
     }
 
+    private class FlingOnGestureListener extends GestureDetector.SimpleOnGestureListener {
+
+        public boolean onDown(MotionEvent e) {
+            // 不允许父组件拦截事件
+            ViewParent parent = getParent();
+            if (parent != null) {
+                parent.requestDisallowInterceptTouchEvent(true);
+            }
+            // 点击时取消所有滚动效果
+            cancelScroll();
+            mLastMoveY = e.getY();
+            mLastMoveX = e.getX();
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, final float velocityY) {
+            // 惯性滑动
+            cancelScroll();
+            fling(getScrollX(), velocityX);
+            return true;
+        }
+    }
+
+    public void cancelScroll() {
+        mLastScrollY = 0;
+        mLastScrollX = 0;
+        mScroller.abortAnimation();
+    }
+
+    // 平滑滚动
+    private void scroll(float from, int to) {
+        mLastScrollX = (int) from;
+        mIsMovingCenter = true;
+        mScroller.startScroll((int) from, 0, 0, 0);
+        mScroller.setFinalX(to);
+        invalidate();
+    }
+
+    // 惯性滑动，
+    private void fling(float from, float vel) {
+        mLastScrollX = (int) from;
+        // 最多可以惯性滑动20个item
+        mScroller.fling((int) from, 0, (int) vel, 0, -20 * (int) mItemWidth,
+                20 * (int) mItemWidth, 0, 0);
+        invalidate();
+    }
+
     public float[] getOnTouch() {
+        float centerX = startX + (endX - startX) / 2 + getScrollX();
         if (scaleLines.size() > 0) {
             for (int i = 0; i < scaleLines.size(); i++) {
                 float x = scaleLines.get(i)[0];
-                if (moveX == x) {
+                if (centerX == x) {
                     return scaleLines.get(i);
                 } else {
-                    if (Math.abs(moveX - x) < itemWidth) {
+                    if (Math.abs(centerX - x) < mItemWidth) {
                         return scaleLines.get(i);
                     } else {
                         continue;
